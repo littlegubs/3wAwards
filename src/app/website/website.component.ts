2a755aa704a800ff784289ad42bc47621e52f2be
lbourgeois <lucasbourgeois60@hotmail.fr>
2018-02-28 21:07:13 +0100

    uncall style css on each components

--- src/app/website/website.component.ts
+++ src/app/website/website.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-website',
   templateUrl: './website.component.html',
-  styleUrls: ['./website.component.scss']
 })
 export class WebsiteComponent implements OnInit {
 
