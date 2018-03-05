730bf9a5c571c6ee18165bc41e53a9b93d2fd8fb
ZPredou <williamdecourty@live.fr>
2018-03-05 20:50:23 +0100

    Changes for PR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,9 @@ export class AppComponent {
   title = 'app';
   param = {value: 'world'};
 
-  constructor(translate: TranslateService) {
-    // this language will be used as a fallback when a translation isn't found in the current language
+  constructor(translate: TranslateService)
     translate.setDefaultLang('en');
 
-     // the lang to use, if the lang isn't available, it will use the current loader to get them
     translate.use('fr');
-
   }
 }
