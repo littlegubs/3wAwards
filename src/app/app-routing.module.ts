b498b51d3862ff6ba0cd325398bc7a1b9c0d886e
Decourty William <williamdecourty@live.fr>
2018-04-05 15:05:06 +0200

    client-profile component done , may need some refactors ( duplication of code )

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './front/home/home.component';
 import {AgencyProfileComponent} from './front/agency-profile/agency-profile.component';
+import {ClientProfileComponent} from './front/client-profile/client-profile.component';
 
 const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'agency', component: AgencyProfileComponent},
+    {path: 'client', component: ClientProfileComponent},
     {path: '**', redirectTo: '', pathMatch: 'full'},
 ];
 
