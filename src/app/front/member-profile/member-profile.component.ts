9bf4fdd3c2c9d71bb21833a31c327a0b5ad16016
lbourgeois <lucasbourgeois60@hotmail.fr>
2018-04-05 17:17:22 +0200

    Fix white border tabs
    To do : fix call sass agency & client components
    and label for tabs with trads

--- src/app/front/member-profile/member-profile.component.ts
+++ src/app/front/member-profile/member-profile.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-member-profile',
   templateUrl: './member-profile.component.html',
-  styleUrls: ['./member-profile.component.scss']
 })
 export class MemberProfileComponent implements OnInit {
 
