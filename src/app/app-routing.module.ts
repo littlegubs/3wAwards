c2d6c706ad2d57c2c45e8cbe63c8c8bcaefee918
Decourty William <williamdecourty@live.fr>
2018-04-05 14:05:25 +0200

    Agency page created almost done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './front/home/home.component';
+import {AgencyProfileComponent} from './front/agency-profile/agency-profile.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  { path: '**',   redirectTo: '', pathMatch: 'full' },
+    {path: '', component: HomeComponent},
+    {path: 'agency', component: AgencyProfileComponent},
+    {path: '**', redirectTo: '', pathMatch: 'full'},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
