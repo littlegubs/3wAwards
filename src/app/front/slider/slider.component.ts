55143b9a496b786e4d74e9a7a38924d8c4b69375
florob95 <florob95@gmail.com>
2018-04-12 22:23:41 +0200

    img slider and icon profile

--- src/app/front/slider/slider.component.ts
+++ src/app/front/slider/slider.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {AwardsService} from '../../../backend/services';
+import {Award} from '../../../backend/model';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
 })
 export class SliderComponent implements OnInit {
+  awards: Award[] = [];
 
-  constructor() { }
+  constructor(private awardsService: AwardsService) { }
 
   ngOnInit() {
+    this.awardsService.getAllByFilter('type', 'day', 1).subscribe(
+      res => {
+        this.awards = res;
+        console.log(this.awards);
+      },
+      err => {
+      }
+    );
   }
 
 }
