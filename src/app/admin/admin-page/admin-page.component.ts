4bec94f4761432af92e7c07d9a62828fdd3da9b1
Decourty William <williamdecourty@live.fr>
2018-05-23 22:36:47 +0200

    Revert "fixed conflict"
    This reverts commit b5eb61d1f3336ce2735e16d74cf3653b2792be03.

---
+++ src/app/admin/admin-page/admin-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router'; 
+import { NbSidebarModule, NbLayoutModule, NbSidebarService } from '@nebular/theme';
+
+@Component({
+  selector: 'app-admin-page',
+  templateUrl: './admin-page.component.html',
+})
+export class AdminPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {FormsModule} from '@angular/forms';
 import {ReactiveFormsModule} from '@angular/forms';
 import {AppRoutingModule} from './app-routing.module';
 import {BootstrapModule} from './bootstrap.module';
+import {NbThemeModule, NbSidebarModule, NbLayoutModule, NbSidebarService} from '@nebular/theme';
 import {
     MatButtonModule,
     MatDialogModule,
@@ -75,6 +76,8 @@ import {StarRatingModule} from 'angular-star-rating';
 import { ClientCardComponent } from './front/client-card/client-card.component';
 import { AgencyCardComponent } from './front/agency-card/agency-card.component';
 import { MemberAwardProjectTableComponent } from './front/member-award-project-table/member-award-project-table.component';
+import { AdminPageComponent } from './admin/admin-page/admin-page.component';
+import {RouterModule} from '@angular/router';
 
 
 export function HttpLoaderFactory(http: HttpClient) {
@@ -115,6 +118,7 @@ export function createRestangularConfigFactory(RestangularProvider) {
     ClientCardComponent,
     AgencyCardComponent,
     MemberAwardProjectTableComponent,
+    AdminPageComponent,
   ],
   imports: [
     BrowserModule,
@@ -152,13 +156,18 @@ export function createRestangularConfigFactory(RestangularProvider) {
         deps: [HttpClient]
       }
     }),
-    StarRatingModule.forRoot()
+    StarRatingModule.forRoot(),
+    NbThemeModule.forRoot({ name: 'default' }),
+      RouterModule,
+      NbLayoutModule,
+      NbSidebarModule,
   ],
   entryComponents: [
     ConnectionDialogComponent,
     RegistrationDialogComponent
   ],
   providers: [
+      NbSidebarService,
     AgenciesService,
     AwardsService,
     CategoriesService,
