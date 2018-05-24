410a304ce427c2a39adca3eb6bbd763e8f6efa53
Alexis Leroy <alexis-60@hotmail.fr>
2018-05-24 18:55:42 +0200

    changed 'vote' button trigger when user is connected or not

--- src/app/front/project-profile/project-profile.component.ts
+++ src/app/front/project-profile/project-profile.component.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {ProjectsService} from '../../../backend/services/Projects.service';
 import {Project} from '../../../backend/model/Project';
 import {MatDialog} from '@angular/material';
-import {ProjectFormVoteComponent} from "../project-form-vote/project-form-vote.component";
+import {ProjectFormVoteComponent} from '../project-form-vote/project-form-vote.component';
+import {AuthService} from '../../auth.service';
+import {MembersService} from '../../../backend/services';
+import {TokenInterface} from '../../tokenInterface';
+import {Member} from '../../../backend/model';
+import {ConnectionDialogComponent} from '../connection-dialog/connection-dialog.component';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-project-profile',
@@ -11,34 +17,58 @@ import {ProjectFormVoteComponent} from "../project-form-vote/project-form-vote.c
 })
 export class ProjectProfileComponent implements OnInit {
 
-    project: Project;
-    toggleAgency = false;
-    toggleMain = false;
-    toggleDev = false;
+  project: Project;
+  tokenStorage = localStorage.getItem('user_token');
+  userInfo: TokenInterface;
+  toggleAgency = false;
+  toggleMain = false;
+  toggleDev = false;
+  member: Member;
 
-    constructor(private route: ActivatedRoute, private projectsService: ProjectsService, public dialog: MatDialog) {
+  constructor(private route: ActivatedRoute, private projectsService: ProjectsService, public dialog: MatDialog,
+              private membersService: MembersService, private authService: AuthService, private http: HttpClient) {
+    this.authService.gettoken().subscribe(res => {
+      this.tokenStorage = res;
+      console.log(this.tokenStorage);
+      this.retrieveMember();
+    });
+    this.retrieveMember();
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.projectsService.get(params.id).subscribe(
+        res => {
+          this.project = res;
+        },
+        err => {
+        }
+      );
+    });
+  }
+
+  openDialog() {
+    let dialogRef;
+    console.log(this.member);
+    if (this.member) {
+      dialogRef = this.dialog.open(ProjectFormVoteComponent, {
+        width: '530px',
+      });
+      dialogRef.afterClosed().subscribe(result => {
+      });
+    } else {
+      dialogRef = this.dialog.open(ConnectionDialogComponent);
     }
+  }
 
-    ngOnInit() {
-        this.route.params.subscribe(params => {
-            this.projectsService.get(params.id).subscribe(
-                res => {
-                    this.project = res;
-                },
-                err => {
-                }
-            );
-        });
+  retrieveMember() {
+    console.log(this.tokenStorage);
+    if (this.tokenStorage) {
+      this.userInfo = this.authService.getUserInfo(this.tokenStorage);
+      this.membersService.get(this.userInfo.id).subscribe(res => {
+        this.member = res;
+      });
     }
-
-    openDialog() {
-        const dialogRef = this.dialog.open(ProjectFormVoteComponent, {
-            width: '530px',
-        });
-
-        dialogRef.afterClosed().subscribe(result => {
-
-        });
-    }
+  }
 
 }
