b6edbab4c4dc2421c53e5f171c730a91018a8bf7
Decourty William <williamdecourty@live.fr>
2018-06-01 10:04:41 +0200

    add agency form

--- src/app/front/add-agency/add-agency.component.ts
+++ src/app/front/add-agency/add-agency.component.ts
@@ -8,111 +8,145 @@ import {MatChipInputEvent} from '@angular/material';
 import {COMMA, ENTER} from '@angular/cdk/keycodes';
 
 @Component({
-  selector: 'app-add-agency',
-  templateUrl: './add-agency.component.html',
-  styleUrls: ['./add-agency.component.scss']
+    selector: 'app-add-agency',
+    templateUrl: './add-agency.component.html',
 })
 export class AddAgencyComponent implements OnInit {
-  agencies: Array<Agency> = [];
-  form: Form<Agency>;
-  tokenStorage = localStorage.getItem('user_token');
-  userInfo: TokenInterface;
-  member: Member;
-  typeTags: TypeTag[] = [];
-  typeAgencies: TypeAgency[] = [];
-  agencyTags: Tag[] = [];
-  idTypeAgency: number;
-  customTags: Tag[] = [];
-  addOnBlur = true;
-  separatorKeysCodes = [ENTER, COMMA];
+    agencies: Array<Agency> = [];
+    form: Form<Agency>;
+    tokenStorage = localStorage.getItem('user_token');
+    userInfo: TokenInterface;
+    member: Member;
+    typeTags: TypeTag[] = [];
+    typeAgencies: TypeAgency[] = [];
+    agencyTags: Tag[] = [];
+    idTypeAgency: number;
+    customTags: Tag[] = [];
+    addOnBlur = true;
+    separatorKeysCodes = [ENTER, COMMA];
 
-  constructor(private agenciesService: AgenciesService, private formService: FormService, private typeTagService: TypeTagsService,
-              private membersService: MembersService, private authService: AuthService, private  typeAgenciesService: TypeAgenciesService) {
-  }
+    constructor(private agenciesService: AgenciesService, private formService: FormService, private typeTagService: TypeTagsService,
+                private membersService: MembersService, private authService: AuthService, private  typeAgenciesService: TypeAgenciesService) {
+    }
 
-  ngOnInit() {
-    this.userInfo = this.authService.getUserInfo(this.tokenStorage);
-    this.getAllTypeTag();
-    // initialize the form with a whole new Agency
-    this.createNewAgency();
-    this.typeAgenciesService.getAll().subscribe(
-      res => {
-        this.typeAgencies = res;
-      },
-      err => {
-      }
-    );
-  }
+    ngOnInit() {
+        this.userInfo = this.authService.getUserInfo(this.tokenStorage);
+        this.getAllTypeTag();
+        // initialize the form with a whole new Agency
+        this.createNewAgency();
+        this.typeAgenciesService.getAll().subscribe(
+            res => {
+                this.typeAgencies = res;
+                console.log(this.typeAgencies);
+            },
+            err => {
+            }
+        );
+    }
 
-  createNewAgency(): void {
-    this.form = this.formService.makeForm<Agency>(new Agency());
-  }
+    createNewAgency(): void {
+        this.form = this.formService.makeForm<Agency>(new Agency());
+    }
 
-  commitAgency(): void {
-    if (this.form.group.dirty && this.form.group.valid) {
-      const newAgency = this.form.get();
-      if (newAgency.id) {
-        this.agenciesService.update(newAgency).subscribe(agency => console.log('yeah!'));
-      } else {
-        newAgency.setProjectsatNull();
-        newAgency.tags = this.agencyTags;
-        newAgency.image = null;
-        if (this.idTypeAgency === undefined) {
-          newAgency.setTypeAgency(this.typeAgencies[0].id);
+    commitAgency(): void {
+        if (this.form.group.dirty && this.form.group.valid) {
+            const newAgency = this.form.get();
+            if (newAgency.id) {
+                this.agenciesService.update(newAgency).subscribe(agency => console.log('yeah!'));
+            } else {
+                newAgency.setProjectsatNull();
+                newAgency.tags = this.agencyTags;
+                newAgency.image = null;
+                if (this.idTypeAgency === undefined) {
+                    newAgency.setTypeAgency(this.typeAgencies[0].id);
+                }
+                newAgency.setTypeAgency(this.idTypeAgency);
+                newAgency.setMember(this.userInfo.id);
+                this.agenciesService.add(newAgency).subscribe(agency => console.log('add'));
+            }
+        } else {
+            // force invalid inputs state to display errors
+            this.form.displayErrors();
         }
-        newAgency.setTypeAgency(this.idTypeAgency);
-        newAgency.setMember(this.userInfo.id);
-        this.agenciesService.add(newAgency).subscribe(agency => console.log('add'));
-      }
-    } else {
-      // force invalid inputs state to display errors
-      this.form.displayErrors();
     }
-  }
 
-  getAllTypeTag() {
-    this.typeTagService.getAll().subscribe(
-      res => {
-        this.typeTags = res;
-      },
-      err => {
-      }
-    );
-  }
+    getAllTypeTag() {
+        this.typeTagService.getAll().subscribe(
+            res => {
+                this.typeTags = res;
+            },
+            err => {
+            }
+        );
+    }
 
-  getTypeAgencies(value): void {
-    this.idTypeAgency = value;
-  }
+    getTypeAgencies(value): void {
+        this.idTypeAgency = value;
+    }
 
-  addTags(event: MatChipInputEvent, type: string): void {
-    if ((event.value || '').trim()) {
-      const tag = new Tag();
-      for (let typeTag of this.typeTags) {
-        if (typeTag.libelle === type) {
-          tag.setType(typeTag.id);
-          tag.type.libelle = type;
+    addTags(event: MatChipInputEvent, type: string): void {
+        if ((event.value || '').trim()) {
+            const tag = new Tag();
+            for (let typeTag of this.typeTags) {
+                if (typeTag.libelle === type) {
+                    tag.setType(typeTag.id);
+                    tag.type.libelle = type;
+                }
+            }
+            tag.libelle = event.value;
+            this.agencyTags.push(tag);
         }
-      }
-      tag.libelle = event.value;
-      this.agencyTags.push(tag);
+        // Reset the input value
+        if (event.input) {
+            event.input.value = '';
+        }
+        this.refreshTagsArray();
+        console.log(this.typeTags);
     }
-    // Reset the input value
-    if (event.input) {
-      event.input.value = '';
-    }
-    this.refreshTagsArray();
-  }
 
-  refreshTagsArray() {
-    this.customTags = this.agencyTags.filter(tag => tag.type.libelle === 'custom');
-  }
+    addTag(value: string, type: string): void {
+        console.log(value);
+        if (value === '') {
+            for (let i = 0; i < this.agencyTags.length; i++) {
+                if (this.agencyTags[i].type.libelle === type) {
+                    this.agencyTags.splice(i, 1);
+                }
+            }
+        }
+        if (value !== '') {
+            let find = false;
+            for (let i = 0; i < this.agencyTags.length; i++) {
+                if (this.agencyTags[i].type.libelle === type) {
+                    this.agencyTags[i].libelle = value;
+                    find = true;
+                }
+            }
+            if (find === false) {
+                const tag = new Tag();
+                for (let typeTag of this.typeTags) {
+                    if (typeTag.libelle === type) {
+                        tag.setType(typeTag.id);
+                        tag.type.libelle = type;
+                    }
+                }
+                tag.libelle = value;
+                this.agencyTags.push(tag);
+                console.log(this.typeTags);
+            }
+        }
 
-  removeTag(value: string): void {
-    for (let i = 0; i < this.agencyTags.length; i++) {
-      if (this.agencyTags[i].libelle === value) {
-        this.agencyTags.splice(i, 1);
-      }
     }
-    this.refreshTagsArray();
-  }
+
+    refreshTagsArray() {
+        this.customTags = this.agencyTags.filter(tag => tag.type.libelle === 'custom');
+    }
+
+    removeTag(value: string): void {
+        for (let i = 0; i < this.agencyTags.length; i++) {
+            if (this.agencyTags[i].libelle === value) {
+                this.agencyTags.splice(i, 1);
+            }
+        }
+        this.refreshTagsArray();
+    }
 }
