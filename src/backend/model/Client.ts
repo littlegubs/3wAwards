fc180517aaa157c88691a7d9067588f26eab0482
Decourty William <williamdecourty@live.fr>
2018-06-05 13:18:16 +0200

    Add client almost done ( please god 🙌 )

--- src/backend/model/Client.ts
+++ src/backend/model/Client.ts
@@ -1,5 +1,12 @@
 import { ClientBase } from './base/ClientBase';
+import {Project} from './Project';
 
 export class Client extends ClientBase {
   id: number;
+
+  setProjectsatNull(): Client {
+    this.projects = new Project();
+    this.projects['@id'] = [];
+    return this;
+  }
 }
