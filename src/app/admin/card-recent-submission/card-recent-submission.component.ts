6285211d22f8b6319fe4b80125dcedab6233a9d5
lbourgeois <lucasbourgeois60@hotmail.fr>
2018-06-09 11:55:15 +0200

    Start dashboard compo filling

--- src/app/admin/card-recent-submission/card-recent-submission.component.ts
+++ src/app/admin/card-recent-submission/card-recent-submission.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {MembersService} from '../../../backend/services/Members.service';
+import {Member} from '../../../backend/model/Member';
 
 @Component({
   selector: 'app-card-recent-submission',
   templateUrl: './card-recent-submission.component.html',
 })
 export class CardRecentSubmissionComponent implements OnInit {
+    members: Member[];
+    pageNumber = 1;
 
-  constructor() { }
+    constructor(private membersServices: MembersService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.membersServices.getAll(this.pageNumber).subscribe(
+            res => {
+                this.members = res;
+            },
+            err => {
+            }
+        );
+    }
 
 }
